328fe182658fb38b7ad7c18db5976d5c21476664
Robbie Gibson <rkgibson2@gmail.com>
2014-03-17 12:46:50 -0400

    added list of users

--- APIdata.py
+++ APIdata.py
@@ -1,14 +1,21 @@
 import json
 import requests
 
+# Gather data for 10 users from the Web API.
+# Robbie Gibson
+
 class APIProblem(Exception):
     pass
 
 api_key = '7F56AE3AB4357C3E54E73235A0ADE818'
-robbie_id = 76561198072761520
-david_id = 76561198046156567
-benjy_id = 76561198044701967
 
+accounts = [{'user': 'robbie', 'id': 76561198072761520},
+            {'user': 'benjy', 'id': 76561198044701967},
+            {'user': 'david', 'id': 76561198046156567},
+            {'user': 'singsing', 'id': 76561197980022982},
+            {'user': 'merlini', 'id': 76561198028025765}]
+
+print accounts
 
 # fetch_match_history for a Steam 64-bit account ID. The input must be a integer.
 def fetch_match_history(account_id):
@@ -29,15 +36,13 @@ def fetch_match_history(account_id):
 
     # if we still have matches left, call again
     while data['results_remaining'] != 0:
-        parameters['start_at_match_id'] = matches[-1]['match_id']
+        # Start at match one less than last match of previous call
+        parameters['start_at_match_id'] = int(matches[-1]['match_id']) - 1
         parameters['date_max'] = matches[-1]['start_time']
 
         data = requests.get(url, params=parameters).json()['result']
 
         new_matches = data['matches']
-
-        # delete the first element, because we already have that one
-        del new_matches[0]
 
         matches = matches + new_matches  
     
@@ -71,5 +76,5 @@ match_id_list = [i['match_id'] for i in id_list]
 
 match_details = fetch_match_details(match_id_list)
 
-with open('robbie_details.txt', 'w') as outfile:
+with open('robbie_details.json', 'w') as outfile:
   json.dump(match_details, outfile)
