1dfebce50bd665ab350c01c8d25d90752ed9c2c6
Robbie Gibson <rkgibson2@gmail.com>
2014-03-16 23:45:44 -0400

    got all match history

--- APIdata.py
+++ APIdata.py
@@ -1,37 +1,61 @@
 import json
 import requests
 
+class APIProblem(Exception):
+    pass
+
 api_key = '7F56AE3AB4357C3E54E73235A0ADE818'
-account_id = '76561198072761520'
+robbie_id = '76561198072761520'
 
-parameters = {
-				'key': api_key,
-				'account_id': account_id
-	     	}
-
-
-
+"""
 url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/'
 data = requests.get(url, params=parameters).text
 data = json.loads(data)  # load a json string into a collection of lists and dicts
 
-print parameters['key']
 
-# print json.dumps(data, indent=2)  # dump an object into a json string
+matches = data['result']['matches']
 
+print json.dumps(matches, indent=2)  # dump an object into a json string
 
-"""def fetch_match_history(account_id): 
-    
-    # we can call the API six times and we'll grab all available data from the API
-    # if account id = 4294967295
-    
+"""
+
+# fetch_match_history for a Steam 64-bit account ID. The input must be a integer.
+def fetch_match_history(account_id):
+
+    if account_id == 4294967295:
+        raise APIProblem ("ID reserved for private account")
+
+    parameters = { 'key': api_key, 'account_id': account_id }
+
     # first API call
-    url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=7F56AE3AB4357C3E54E73235A0ADE818&date_max&account_id=' + account_id
-    data = requests.get(url).text
-    data = json.loads(data)
-    
-    df = pd.DataFrame.from_dict(data)    
-    
-    return df
+    url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/'
+    data = requests.get(url, params=parameters).json()['result']
 
-fetch_match_history(account_id) """
+    if data['status'] == 15:
+        raise PrivateID ("Private or non-existent account")
+
+    matches = data['matches']
+
+    # if we still have matches left, call again
+    while data['results_remaining'] != 0:
+        parameters['start_at_match_id'] = matches[-1]['match_id']
+
+        data = requests.get(url, params=parameters).json()['result']
+
+        new_matches = data['matches']
+
+        # delete the first element, because we already have that one
+        del new_matches[0]
+
+        matches = matches + new_matches
+    
+    # df = pd.DataFrame.from_dict(data)    
+    
+    return matches
+
+try:
+    value = fetch_match_history(robbie_id)
+except APIProblem, e:
+    print e
+
+print len(value)
