6dc18fe54af3c5790d8907dcc16fad1957683959
Robbie Gibson <rkgibson2@gmail.com>
2014-03-17 00:17:42 -0400

    python parsing works

--- APIdata.py
+++ APIdata.py
@@ -5,19 +5,9 @@ class APIProblem(Exception):
     pass
 
 api_key = '7F56AE3AB4357C3E54E73235A0ADE818'
-robbie_id = '76561198072761520'
+robbie_id = 76561198072761520
+david_id = 76561198046156567
 
-"""
-url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/'
-data = requests.get(url, params=parameters).text
-data = json.loads(data)  # load a json string into a collection of lists and dicts
-
-
-matches = data['result']['matches']
-
-print json.dumps(matches, indent=2)  # dump an object into a json string
-
-"""
 
 # fetch_match_history for a Steam 64-bit account ID. The input must be a integer.
 def fetch_match_history(account_id):
@@ -32,13 +22,14 @@ def fetch_match_history(account_id):
     data = requests.get(url, params=parameters).json()['result']
 
     if data['status'] == 15:
-        raise PrivateID ("Private or non-existent account")
+        raise APIProblem ("Private or non-existent account")
 
     matches = data['matches']
 
     # if we still have matches left, call again
     while data['results_remaining'] != 0:
         parameters['start_at_match_id'] = matches[-1]['match_id']
+        parameters['date_max'] = matches[-1]['start_time']
 
         data = requests.get(url, params=parameters).json()['result']
 
@@ -47,15 +38,35 @@ def fetch_match_history(account_id):
         # delete the first element, because we already have that one
         del new_matches[0]
 
-        matches = matches + new_matches
-    
-    # df = pd.DataFrame.from_dict(data)    
+        matches = matches + new_matches  
     
     return matches
 
 try:
-    value = fetch_match_history(robbie_id)
+    id_list = fetch_match_history(robbie_id)
 except APIProblem, e:
     print e
 
-print len(value)
+# fetch match details for a list of match_ids
+def fetch_match_details(match_ids):
+
+    parameters = { 'key': api_key}
+    url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/'
+
+    match_list = []
+
+    for match in match_ids:
+        print match
+        parameters['match_id'] = match
+        data = requests.get(url, params=parameters).json()['result']
+
+        if 'error' in data:
+            raise APIProblem ("Non-existent match")
+
+        match_list = match_list + [data]
+
+    return match_list
+
+match_id_list = [i['match_id'] for i in id_list]
+
+match_details = fetch_match_details(match_id_list)
