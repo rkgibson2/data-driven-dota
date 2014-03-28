f20fdd5a674dcff96492cb19b61062e70bb18e59
Robbie Gibson <rkgibson2@gmail.com>
2014-03-27 20:59:21 -0400

    removed pretty-printing

--- match_data/fix.py
+++ match_data/fix.py
@@ -11,4 +11,4 @@ with open('match_details.json', 'r') as json_data:
     match_details = json.load(json_data)
     for key, value in match_details.items():
         with open(key + '_match_details.json', 'w') as outfile:
-            json.dump(value, outfile, indent=4, sort_keys=True)
+            json.dump(value, outfile, sort_keys=True)
