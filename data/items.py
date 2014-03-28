4e53b9195d1ae09e507943a8be06a904ef6f4f3c
Robbie Gibson <rkgibson2@gmail.com>
2014-03-28 17:14:17 -0400

    pulled Valve's item data

---
+++ data/items.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import time
+
+data = requests.get('http://www.dota2.com/jsfeed/itemdata').json()['itemdata']
+
+with open('items.json', 'r+') as item_file:
+    item_list = json.load(item_file)['items']
+
+for key, value in data.items():
+    for item in item_list:
+        if key == item['name']:
+            for key2, value2 in value.items():
+                item[key2] = value2
+
+            print item.keys()
+
+with open('items.json', 'w') as outfile:
+    json.dump(item_list, outfile, indent=4)
