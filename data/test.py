3ec1ece3dc32f7da98950cb1f1d078681bab4d0f
Robbie Gibson <rkgibson2@gmail.com>
2014-03-30 20:18:49 -0400

    set up ability file, with id's and images

--- data/test.py
+++ data/test.py
@@ -1,13 +1,18 @@
 import json
 import requests
 
-with open('heroes.json', 'r+') as hero_file:
-    hero_list = json.load(hero_file)
+ability_list = requests.get('https://raw.githubusercontent.com/kronusme/dota2-api/master/data/abilities.json').json()['abilities']
 
-for hero in hero_list:
-    if 'img' in hero:
-        hero['img'] = '/' + hero['img']
-        print hero['img']
 
-with open('heroes.json', 'w') as outfile:
-    json.dump(hero_list, outfile, indent=4)
+with open('abilities.json', 'r') as ability_file:
+    data = json.load(ability_file)
+
+for key, value in data.items():
+    for ability in ability_list:
+        if key == ability['name']:
+            for key2, value2 in value.items():
+                ability[key2] = value2
+
+
+with open('abilities.json', 'w') as outfile:
+     json.dump(ability_list, outfile, sort_keys=True, indent=4)
