0abc6f9ff8f4d4c2c24f26c492153eb1864840ef
Benjy Levin <benjylevin@college.harvard.edu>
2014-04-01 18:58:44 -0400

    rewrote selector js to make use of getKeys and 3 diff arrays. Heroes now sorted, and filter returns hero ID instead of name. Now linking to pictures from hero.img

--- js/selectors.js
+++ js/selectors.js
@@ -1,61 +1,80 @@
-d2.loadJson(function(){
-
-//herolist = new Array();
-hero_keys = d2.getKeys("heroes");
-
-for(var i=1;  i<hero_keys.length; i++)
+d2.loadJson(function ()
 {
-     var hero =  d2.getHeroInfo(hero_keys[i]);;
-     if (hero)
-     {
-        var herodname = hero.dname.toLowerCase().replace(/ /g,"_").replace(/'/g,"");
-        if (hero.stat == "strength")
-        {
-             var heroname = hero.name;//.toLowerCase().replace(/ /g,"_");
-             d3.select("#strimages").append("img").attr("id",heroname).attr("class","pic");
-             d3.select("#strimages").select("#" + heroname).attr('src', function() { return "../img/heroes/" +herodname+".jpg"; }).attr("width","80px").attr("value",herodname);
-	d3.select("#strimages").select("#"+heroname).on("click", highlight);
+	hero_keys = d2.getKeys("heroes");
+	var intheroes = new Array();
+	var agiheroes = new Array();
+	var strheroes = new Array();
+	for (var i = 1; i < hero_keys.length; i++)
+	{
+		var hero = d2.getHeroInfo(hero_keys[i]);;
+		if (hero)
+		{
+			if (hero.stat == "strength")
+			{
+				strheroes.push(hero);
+			}
+			else if (hero.stat == "agility")
+			{
+				agiheroes.push(hero);
+			}
+			else if (hero.stat == "intelligence")
+			{
+				intheroes.push(hero);
+			}
+		}
+	};
+	strheroes.sort(sorting);
+	agiheroes.sort(sorting);
+	intheroes.sort(sorting);
+	strheroes.forEach(function (d)
+	{
+		var heroname = d.name;
+		d3.select("#strimages").append("img").attr("id", heroname).attr("class", "pic");
+		d3.select("#strimages").select("#" + heroname).attr('src', d.img).attr("width", "80px").attr("value", d.id);
+		d3.select("#strimages").select("#" + heroname).on("click", highlight);
+	});
+	agiheroes.forEach(function (d)
+	{
+		var heroname = d.name;
+		d3.select("#agiimages").append("img").attr("id", heroname).attr("class", "pic");
+		d3.select("#agiimages").select("#" + heroname).attr('src', d.img).attr("width", "80px").attr("value", d.id);
+		d3.select("#agiimages").select("#" + heroname).on("click", highlight);
+	});
+	intheroes.forEach(function (d)
+	{
+		var heroname = d.name;
+		d3.select("#intimages").append("img").attr("id", heroname).attr("class", "pic");
+		d3.select("#intimages").select("#" + heroname).attr('src', d.img).attr("width", "80px").attr("value", d.id);
+		d3.select("#intimages").select("#" + heroname).on("click", highlight);
+	});
+});
+
+function highlight()
+{
+	if (!this.classList.contains("selected"))
+	{
+		d3.select(this).attr("class", "pic selected");
+		d3.select(this).style("border", "2px solid red");
 	}
-         if (hero.stat == "agility")
-        {
-             var heroname = hero.name;//.toLowerCase().replace(/ /g,"_");
-             d3.select("#agiimages").append("img").attr("id",heroname).attr("class","pic");
-             d3.select("#agiimages").select("#" + heroname).attr('src', function() { return "../img/heroes/" +herodname+".jpg"; }).attr("width", "80px").attr("value",herodname);;
-	    d3.select("#agiimages").select("#"+heroname).on("click", highlight); 
-        } 
-         if (hero.stat == "intelligence")
-        {
-             var heroname = hero.name;//.toLowerCase().replace(/ /g,"_");
-             d3.select("#intimages").append("img").attr("id",heroname).attr("class","pic");
-             d3.select("#intimages").select("#" + heroname).attr('src', function() { return "../img/heroes/" +herodname+".jpg"; }).attr("width", "80px").attr("value",herodname);; 
-	d3.select("#intimages").select("#"+heroname).on("click", highlight);
-        }  
-     }
+	else
+	{
+		$(this).removeClass("selected");
+		d3.select(this).style("border", "2px solid black");
+	}
 }
 
-});
-
-function highlight(){
-	
-		if (!this.classList.contains("selected"))
-		{
-			d3.select(this).attr("class","pic selected");
-			d3.select(this).style("border","2px solid red");
-		}
-		else
-		{
-			$(this).removeClass("selected");
-			d3.select(this).style("border","2px solid black");
-		}
+function selected()
+{
+	var selectedheroes = d3.selectAll(".selected")[0];
+	selectedarr = new Array();
+	selectedheroes.forEach(function (d)
+	{
+		selectedarr.push(d.getAttribute("value"));
+	});
+	console.log(selectedarr);
 }
 
-function selected(){
-var selectedheroes = d3.selectAll(".selected")[0];
-selectedarr = new Array ();
-selectedheroes.forEach(function(d){
-// decide how we want to represent the selection? 
-// array?
-selectedarr.push(d.getAttribute("value"));
-});
-console.log(selectedarr);
-}
+function sorting(a, b)
+{
+	return a.dname.localeCompare(b.dname)
+};
