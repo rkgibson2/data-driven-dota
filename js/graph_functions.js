96260f383f746fc1c6504019b6c4a814f588e91f
huihuifan <anarfish@gmail.com>
2014-04-01 01:30:57 -0400

    still working on chord diagram

--- js/graph_functions.js
+++ js/graph_functions.js
@@ -4,7 +4,7 @@
 var user_data;
 
 //margins and bounding boxes for each graph visualization
-var bb_win_loss, bb_hero_pie, bb_item_percent;
+var bb_win_loss, bb_hero_pie, bb_item_percent, bb_hero_chord;
 
 var margin = {
     top: 50,
@@ -38,6 +38,14 @@ bb_item_percent = {
     h: 300
 };
 
+bb_hero_chord = {
+    x: 350,
+    y: 350,
+    w: 400,
+    h: 400
+};
+
+
 //set up those boxes
 svg = d3.select("#stat_graphs").append("svg").attr({
 	width: width + margin.left + margin.right,
@@ -53,12 +61,17 @@ var win_loss_graph = svg.append("g")
 
 var hero_pie_graph = svg.append("g")
 	.attr("class", "hero_pie")
-	.attr("transform", "translate(" + (bb_hero_pie.x + (bb_hero_pie.w / 2)) + "," + (bb_hero_pie.y+(bb_hero_pie.h / 2 + 10)) + ")");;
+	.attr("transform", "translate(" + (bb_hero_pie.x + (bb_hero_pie.w / 2)) + "," + (bb_hero_pie.y+(bb_hero_pie.h / 2 + 10)) + ")");
 
 var item_percent_graph = svg.append("g")
 	.attr("class", "item_percent")
 	.attr("visibility", "hidden")
-	.attr("transform", "translate(" + bb_item_percent.x + "," + bb_item_percent.y + ")");;
+	.attr("transform", "translate(" + bb_item_percent.x + "," + bb_item_percent.y + ")");
+
+var hero_chord_graph = svg.append("g")
+	.attr("class", "hero_chord")
+	//.attr("visibility", "hidden")
+	.attr("transform", "translate(" + (bb_hero_chord.x+(bb_hero_chord.w/2)) + "," + (bb_hero_chord.y +(bb_hero_chord.h / 2)) + ")");
 
 var item_percent_x, item_percent_y, item_percent_xAxis, item_percent_yAxis, item_percent_color;
 
@@ -70,11 +83,16 @@ var tip = d3.tip()
 svg.call(tip);
 
 //function calls
-loadData("robbie");
+d2.loadJson(function() {
+	loadData("robbie");
+});
+
+//loadData("robbie");
 
 draw_win_loss();
 
 draw_item_percent();
+
 
 function loadData(username) {
 	 
@@ -88,6 +106,8 @@ function loadData(username) {
         update_item_percent(user_data);
 
         create_flare(data);
+
+        create_matrix(data);
 
     })
 }
@@ -326,12 +346,7 @@ function create_flare(data) {
 
 			if (current_hero_stat == "agility") {
 				var cur = hero_flare.children[0].children;
-				//console.log(cur)
-				//console.log(d2.getHeroName(d.player_info.hero_id))
 				for (var i=0; i < cur.length; i++) {
-					if (d.player_info.hero_id == 0) {
-						console.log(d)
-					}
 					if (cur[i].dname == d2.getHeroName(d.player_info.hero_id)) {
 						cur[i].count += 1;
 					}
@@ -359,6 +374,8 @@ function create_flare(data) {
 			}
 
 		})
+
+		//console.log(hero_flare)
 	
 		hero_pie(hero_flare);
 
@@ -672,5 +689,372 @@ function update_item_percent(data) {
 	// $('#cvote').slider('refresh');
 
 
+}
+
+function k_combinations(set, k) {
+	var i, j, combs, head, tailcombs;
+	
+	if (k > set.length || k <= 0) {
+		return [];
+	}
+	
+	if (k == set.length) {
+		return [set];
+	}
+	
+	if (k == 1) {
+		combs = [];
+		for (i = 0; i < set.length; i++) {
+			combs.push([set[i]]);
+		}
+		return combs;
+	}
+		
+	combs = [];
+	for (i = 0; i < set.length - k + 1; i++) {
+		head = set.slice(i, i+1);
+		tailcombs = k_combinations(set.slice(i + 1), k - 1);
+		for (j = 0; j < tailcombs.length; j++) {
+			combs.push(head.concat(tailcombs[j]));
+		}
+	}
+	return combs;
+}
+
+
+function reset_index(hero_id) {
+
+	//hero id 107 is now 0
+	//hero id 109 is not 24
+	//hero id 110 is now 105
+	//every other hero id is mapped to the correct index
+	//when labeling the chord diagram, relabel hero ids 107, 109, 110
+
+	if (hero_id == 109) {
+		return 24;
+	}
+	if (hero_id == 110) {
+		return 105;
+	}
+	if (hero_id == 107) {
+		return 0;
+	}
+	else {
+		return hero_id;
+	}
+}
+
+function create_matrix (data) {
+
+	var matrix = [];
+
+	for (var k = 0; k < 107; k++) {
+		var new_array = [];
+		for (var l = 0; l < 107; l++) {
+			new_array.push(0);
+		}
+		matrix.push(new_array);
+	}
+
+	// var matrix_obj = {};
+
+	// for (var i = 0; i < 107; i++) {
+	// 	matrix_obj[i] = {};
+	// 	for (var j = 0; j < 107; j++) {
+	// 		matrix_obj[i][j] = 0;
+	// 	}
+	// }
+
+	for (var i = 0; i < data.matches.length; i++) {
+		var set = [];
+		var gamemode = data.matches[i].game_mode
+
+		//filter out gamemodes we don't want, like ones with five players, etc.
+		if (gamemode == 15 || gamemode == 7 || gamemode == 0 || 
+			gamemode == 6 || gamemode == 8 || gamemode == 9 || 
+			gamemode == 10 || gamemode == 11 || gamemode == 12 || 
+			gamemode == 13 || gamemode == 14 || gamemode ==  15 || gamemode == 18) {
+			continue;
+		}
+
+		for (var j = 0; j < 9; j++) {
+			set.push(data.matches[i].players[j].hero_id);
+		}
+
+		var combinations_rad = k_combinations(set.slice(0,5),2);
+		var combinations_dire = k_combinations(set.slice(5,10),2);
+
+		for (var c = 0; c < combinations_rad.length; c ++) {
+			matrix[reset_index(combinations_rad[c][0])][reset_index(combinations_rad[c][1])] += 1; 
+			matrix[reset_index(combinations_rad[c][1])][reset_index(combinations_rad[c][0])] += 1; 
+		}
+		for (var c = 0; c < combinations_dire.length; c ++) {
+			matrix[reset_index(combinations_dire[c][0])][reset_index(combinations_dire[c][1])] += 1; 
+			matrix[reset_index(combinations_dire[c][1])][reset_index(combinations_dire[c][0])] += 1; 
+		}
+	}
+
+	var new_dict = {};
+
+	for (var y = 0; y < 107; y ++) {
+		for (var z = 0; z < 107; z++) {
+			// if (matrix[y][z] != 0) {
+			// 	new_dict[y] = {};
+			// 	new_dict[y][z] = matrix[y][z]
+			// }
+			// if (matrix[z][y] != 0) {
+			// 	new_dict[z] = {};
+			// 	new_dict[z][y] = matrix[z][y]
+			// }
+			if (matrix[y][z] < 7) {
+				matrix[y][z] = 0
+			}
+		}
+	}
+
+	console.log(new_dict)
+	var dense_matrix = [];
+
+	for (var y = 0; y < Object.keys(new_dict).length; y ++) {
+		var new_array = [];
+		//console.log(new_dict[y]);
+
+		for (var key in new_dict[y]) {
+		  new_array.push(new_dict[y][key])
+		}
+
+		dense_matrix.push(new_array);
+	}
+	console.log(dense_matrix)
+
+	draw_hero_chord_graph(matrix);
+}
+
+function draw_hero_chord_graph(matrix) {
+
+	var chord = d3.layout.chord()
+	    .padding(.05)
+	    .sortSubgroups(d3.descending)
+	    .matrix(matrix);
+
+	var hero_chord_width = bb_hero_chord.w,
+	    hero_chord_height = bb_hero_chord.h,
+	    innerRadius = Math.min(hero_chord_width, hero_chord_height) * .41,
+	    outerRadius = innerRadius * 1.1;
+
+	var fill = d3.scale.ordinal()
+	    .domain(d3.range(4))
+	    .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+
+	var groupings = {};
+	for (var i = 0; i < 106; i++) {
+		if (chord.groups()[i].value != 0) {
+			//console.log(chord.groups()[i].endAngle)
+			//chord.groups()[i].endAngle = chord.groups()[i].endAngle + (chord.groups()[i].value*.01)
+			groupings[i] = chord.groups()[i].value;
+		}
+	}
+
+	//console.log(groupings)
+
+	var padding = 0.05;
+
+	var k = 0;
+
+	for(var i in Object.keys(groupings)) { 
+		if (groupings[String(i)]) {
+			k += groupings[i]; 
+			//console.log(k)
+		}
+	}
+
+	var ngroups = Object.keys(groupings).length;
+	
+	k = ((2 * 3.1415 - padding * ngroups) / k);
+
+	var connections = [];
+	for (var i in Object.keys(groupings)) {
+		if (groupings[String(i)]) {
+			connections.push([{group: parseInt(i), value: groupings[i]}])
+		}
+	}
+
+	//console.log(connections[1].length)
+
+	var polygons = [];
+	var poly = {edges: [],
+	            vertices: {}};
+	 
+	var i, j;
+	i = -1; while (++i < connections.length) {
+	    if (connections[i].length >= 1) {
+	        j = 0; while (++j < connections[i].length) {
+	            poly.edges.push({
+	                                source: connections[i][j-1],
+	                                target: connections[i][j]
+	                            });
+	            poly.vertices[connections[i][j-1].group + ''] = '';
+	        }
+	        poly.vertices[connections[i][j-1].group + ''] = '';
+	        if (poly.edges.length >= 1) {
+	            poly.edges.push({
+	                            source: connections[i][0],
+	                            target: connections[i][j-1]
+	                            });
+	        }
+	        polygons.push(poly);
+	        poly = {edges: [],
+	                vertices: {}};
+	    }
+	};
+	 
+	console.log(polygons)
+
+	var h, samebase, subgroups = [];
+ 
+	i = -1; while (++i < ngroups) {
+	    subgroups[i] = [];
+	    j = -1; while (++j < polygons.length) {
+	        samebase = {'ribbons': [],
+	                    'basevalue': 0};
+	        h = -1; while (++h < polygons[j].edges.length) {
+	            if (polygons[j].edges[h].source.group === i) {
+	                samebase.ribbons.push(polygons[j].edges[h]);
+	                samebase.basevalue = polygons[j].edges[h].source.value;
+	            } else if (polygons[j].edges[h].target.group === i) {
+	                samebase.ribbons.push(polygons[j].edges[h]);
+	                samebase.basevalue = polygons[j].edges[h].target.value;
+	            }
+	        }
+	        subgroups[i].push(samebase);
+	    }
+	}
+
+	console.log(subgroups)
+
+	var range = function(n) {
+	    var out = [], i = -1;
+	    while(++i<n) {out.push(i);}
+	    return out;
+	}
+
+	var subgroupIndex = [];
+	i = -1; while (++i < ngroups) {
+    	subgroupIndex.push(range(subgroups[i].length));
+	}
+
+	var pt, pt1, pt2, groups = [];
+ 
+ 	var groupIndex = range(ngroups);
+
+	var x = 0, i = -1; while (++i < ngroups) {
+	    var di = groupIndex[i];
+	    var x0 = x, j = -1; while (++j < subgroupIndex[di].length) {
+	        var dj = subgroupIndex[di][j],
+	        v = subgroups[di][dj].basevalue,
+	        a0 = x,
+	        a1 = x += v * k;
+	        h = -1; while(++h < subgroups[di][dj].ribbons.length) {
+	            pt1 = subgroups[di][dj].ribbons[h].source;
+	            pt2 = subgroups[di][dj].ribbons[h].target;
+	            if (pt1.group === di) { pt = pt1; }
+	            else { pt = pt2; }
+	            pt['geometry'] = {
+	                index: di,
+	                subindex: dj,
+	                startAngle: a0,
+	                endAngle: a1,
+	                value: v
+	            };
+	        }
+	    }
+	    groups[di] = {
+	        index: di,
+	        startAngle: x0,
+	        endAngle: x,
+	        value: (x - x0) / k
+	    };
+	    x += padding;
+	}
+
+	console.log(groups)
+
+	var chords = [];
+ 
+	i = -1; while (++i < polygons.length) {
+	    j = -1; while (++j < polygons[i].edges.length) {
+	        var source = polygons[i].edges[j].source.geometry,
+	        target = polygons[i].edges[j].target.geometry;
+	        if (source.value || target.value) {
+	            chords.push(source.value < target.value
+	                        ? {source: target, target: source, groups: polygons[i].vertices}
+	                        : {source: source, target: target, groups: polygons[i].vertices});
+	        }
+	    }
+	}
+
+	// hero_chord_graph.append("g").selectAll("path")
+	//     .data(groupings)
+	//   .enter().append("path")
+	//     .style("fill", function(d) { return fill(d.index); })
+	//     .style("stroke", function(d) { return fill(d.index); })
+	//     .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+	//     .on("mouseover", fade(.1))
+	//     .on("mouseout", fade(1));
+
+	// var ticks = hero_chord_graph.append("g").selectAll("g")
+	//     .data(groupings)
+	//   .enter().append("g").selectAll("g")
+	//     .data(groupTicks)
+	//   .enter().append("g")
+	//     .attr("transform", function(d) {
+	//       return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+	//           + "translate(" + outerRadius + ",0)";
+	//     });
+
+	// ticks.append("line")
+	//     .attr("x1", 1)
+	//     .attr("y1", 0)
+	//     .attr("x2", 5)
+	//     .attr("y2", 0)
+	//     .style("stroke", "#000");
+
+	// ticks.append("text")
+	//     .attr("x", 8)
+	//     .attr("dy", ".35em")
+	//     .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+	//     .style("text-anchor", function(d, i) { return d.angle > Math.PI ? "end" : null; })
+	//     .text(function(d,i) { return "0"; });
+
+	// hero_chord_graph.append("g")
+	//     .attr("class", "chord")
+	//   .selectAll("path")
+	//     .data(chord.chords)
+	//   .enter().append("path")
+	//     .attr("d", d3.svg.chord().radius(innerRadius))
+	//     .style("fill", function(d) { return fill(d.target.index); })
+	//     .style("opacity", 1);
+
+	// // Returns an array of tick angles and labels, given a group.
+	// function groupTicks(d) {
+	// 	//console.log(d)
+	//   var k = (d.endAngle - d.startAngle) / d.value;
+	//   return d3.range(0, d.value, 1000).map(function(v, i) {
+	//     return {
+	//       angle: v * k + d.startAngle,
+	//       label: i % 5 ? null : v / 1000 + "k"
+	//     };
+	//   });
+	// }
+
+	// // Returns an event handler for fading a given chord group.
+	// function fade(opacity) {
+	//   return function(g, i) {
+	//     svg.selectAll(".chord path")
+	//         .filter(function(d) { return d.source.index != i && d.target.index != i; })
+	//       .transition()
+	//         .style("opacity", opacity);
+	//   };
+	// }
 }
 
