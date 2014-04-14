fa38edc43b1a133e88075a284df701c163c0d0be
Robbie Gibson <rkgibson2@gmail.com>
2014-04-13 20:17:58 -0400

    set default user dropdown to not angela

--- js/user.js
+++ js/user.js
@@ -1,9 +1,17 @@
 users = ["angela","aui_2000","benjy","david","dendi","merlini", "robbie"];
 
 d3.select("#selectuser")
-    .append("select").attr("id","userdropdown").on("change",function(){loadData( d3.select("#userdropdown")
-    .node().value)}).selectAll("option")
-    .data(users).enter().append("option")
-    .attr("value", function(d){ return d})
+    .append("select")
+    .attr("id","userdropdown")
+    .on("change", function() {
+        loadData(d3.select("#userdropdown")
+        .node().value)
+    })
+    .selectAll("option")
+    .data(users)
+    .enter().append("option")
+    .attr("value", function(d){ return d })
     .text(function(d){ return d });
 
+// set default value to robbie (ME!!)
+d3.select("[value=robbie]").property("selected", true)
