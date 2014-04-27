10477a2ded9bfe2e9604a27f9a45fd0b39c0e835
BenjyLevin <benjylevin@college.harvard.edu>
2014-04-27 14:33:28 -0400

    change path to css

--- js/colorblind.js
+++ js/colorblind.js
@@ -12,7 +12,7 @@
                 var newlink = document.createElement("link");
                 newlink.setAttribute("rel", "stylesheet");
                 newlink.setAttribute("type", "text/css");
-                newlink.setAttribute("href", "/css/" + cssFile);
+                newlink.setAttribute("href", "css/" + cssFile);
                 newlink.setAttribute("id", "newcolors")
 
                 document.getElementsByTagName("head").item(0).appendChild(newlink);
